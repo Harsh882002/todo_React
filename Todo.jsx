c76597b306b455d832af9f51d61eff3cfc723a12
Harsh Patle <harshpatle491@gmail.com>
2024-09-30 19:51:40 +0530

    ok

---
+++ Todo.jsx
@@ -0,0 +1,19 @@
+
+export const Todo = () => {
+
+    return (
+
+        <section>
+            <header>
+                <h1>Todo List</h1>
+            </header>
+
+
+            <section>
+                <form >
+                    
+                </form>
+            </section>
+        </section>
+    )
+}
