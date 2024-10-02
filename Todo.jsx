1bb7d8a5f5a4e2bcd333c97eb4012dc3710d1e31
Harsh Patle <harshpatle491@gmail.com>
2024-10-02 13:41:30 +0530

    new

--- Todo.jsx
+++ Todo.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import { MdFileDownloadDone ,MdDeleteForever  } from "react-icons/md";
+import { useEffect, useState } from 'react';
+import { MdFileDownloadDone, MdDelete } from "react-icons/md";
 
 import './Todo.css';
 export const Todo = () => {
 
     const [InputValue, setInputValue] = useState("");
     const [task, setTask] = useState([]);
+    const [dateTime, setDateTime] = useState("");
 
     const handleChange = (event) => {
         setInputValue(event)
@@ -26,6 +27,21 @@ export const Todo = () => {
         setInputValue(" ");
     }
 
+
+    //Time code
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const forMattedDate = now.toLocaleDateString();
+            const forMattedTime = now.toLocaleTimeString();
+
+            setDateTime(`${forMattedDate} - ${forMattedTime}`);
+        }, 1000)
+        return () => clearInterval(interval);
+    }, []);
+
+
+
     return (
 
         <section className='todo-container'>
@@ -33,6 +49,9 @@ export const Todo = () => {
                 <h1 className='todo-header'>Todo List</h1>
             </header>
 
+            <div>
+                <h1 className='date'>{dateTime}</h1>
+            </div>
 
             <section>
                 <form onSubmit={handleFormSubmit} >
@@ -56,13 +75,15 @@ export const Todo = () => {
                     {task.map((curTask, index) => {
                         return (
                             <li key={index} className='todo-item'>
-                                <span>{curTask}</span>
+                                <span className='item-text'>{curTask}</span>
                                 <button className='check-btn'>
-                                <MdFileDownloadDone />
-
+                                    <MdFileDownloadDone />
                                 </button>
 
                                 <button className='delete-btn'>
+                                    <MdDelete />
+
+                                </button>
                             </li>
                         )
                     })}
